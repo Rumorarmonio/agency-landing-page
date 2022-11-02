c93b5f759fe089a984754e016bfebdb1cdfd6ba6
Egor Skleynov <yxllwllxy@gmail.com>
2022-11-02 21:07:24 +0800

    products section added and BEM methodology implemented

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-let navLinks = document.querySelectorAll(".nav_link");
+let navLinks = document.querySelectorAll(".menu__link"),
+    productLinks = document.querySelectorAll(".product-category");
 
 navLinks.forEach((elem) =>
     elem.addEventListener("click", function () {
-        navLinks.forEach((elem) => elem.classList.remove("nav_active"));
-        this.classList.add("nav_active");
+        navLinks.forEach((elem) => elem.classList.remove("menu__link_active"));
+        this.classList.add("menu__link_active");
+    })
+);
+
+productLinks.forEach((elem) =>
+    elem.addEventListener("click", function () {
+        productLinks.forEach((elem) => elem.classList.remove("product-category_active"));
+        this.classList.add("product-category_active");
     })
 );
